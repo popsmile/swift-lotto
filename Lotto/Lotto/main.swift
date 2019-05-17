4ccc998847cedf24bb880f8840f6981ad046435b
jamie.day <jamie.day@kakaopaycorp.com>
2019-05-17 11:04:04 +0900

    로또 프로젝트 생성

---
+++ Lotto/Lotto/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lotto
+//
+//  Created by jamie.day on 17/05/2019.
+//  Copyright © 2019 jamie.day. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
