cd85a5a7ede87ff9c597f5a477cf9ea7473cde28
jamie.day <jamie.day@kakaopaycorp.com>
2019-05-17 11:25:46 +0900

    로또 구입 개수 테스트 구현

---
+++ Lotto/Lotto/Lotto.swift
@@ -0,0 +1,17 @@
+//
+//  Lotto.swift
+//  Lotto
+//
+//  Created by jamie.day on 17/05/2019.
+//  Copyright © 2019 jamie.day. All rights reserved.
+//
+
+import Foundation
+
+class Lotto {
+    let count: Int
+    
+    init(money: Int) {
+        self.count = money / 1000
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# swift-lotto
-로또 게임으로 연습하는 TDD
+# Swift Lotto Game by TDD
+
+
+## 기능 요구사항 분석
+
+- 금액 입력 —> 로또 1장당 1000원 구매
+
